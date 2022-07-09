96f4807576e14ff503b861d9c9475209fb01b5af
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-09 16:39:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Marvel Calendar</h1>
+      <button
+        onClick={() => {
+          alert("Assemble!!!");
+        }}
+      >
+        Avengers?
+      </button>
+    </div>
+  );
+}
+
+export default App;
