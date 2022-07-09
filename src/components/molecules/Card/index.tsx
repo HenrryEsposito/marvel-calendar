ae47d3da9ac2bbd8efbf4a0378f95deafab2a90a
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-09 19:07:03 -0300

    feat - new card component

---
+++ src/components/molecules/Card/index.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+import MuiCard from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
+
+export interface ICard {
+  title: string;
+  description: string;
+  childrenActions?: ReactNode;
+}
+
+export default function Card({ title, description, childrenActions }: ICard) {
+  return (
+    <MuiCard>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {childrenActions && <CardActions>{childrenActions}</CardActions>}
+    </MuiCard>
+  );
+}
