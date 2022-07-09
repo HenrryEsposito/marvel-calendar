38db95e644abeed800a3cd866c1a5af4beb981f8
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-09 19:10:00 -0300

    chore - test calendar early implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Calendar from "./components/organisms/Calendar";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       >
         Avengers?
       </button>
+      <Calendar></Calendar>
     </div>
   );
 }
