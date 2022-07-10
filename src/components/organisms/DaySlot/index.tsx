7be258b2d9e54e694c5efa6d2ea74177ebeddc03
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-09 23:51:16 -0300

    feat - implement draggable capabilities

--- src/components/organisms/DaySlot/index.tsx
+++ src/components/organisms/DaySlot/index.tsx
@@ -1,19 +1,33 @@
 import * as React from "react";
+import { v4 as uniqueId } from "uuid";
 
 import GridItem, { IGridItem } from "../../atoms/GridItem";
 import DaySlotHeader from "../../atoms/DaySlotHeader";
 
 import { Container } from "./styles";
 
+import useDraggable from "../../../hooks/useDraggable";
+
 export interface IDaySlot extends IGridItem {
   dayNumber: number;
 }
 
 export default function DaySlot(props: IDaySlot) {
+  const nodeId = uniqueId();
+
+  const { drop, dragOver } = useDraggable();
+
   return (
-    <Container>
+    <Container
+      onDrop={(e) => {
+        drop(e, nodeId);
+      }}
+      onDragOver={dragOver}
+    >
       <DaySlotHeader dayNumber={props.dayNumber} />
-      <GridItem {...props}>{props.children}</GridItem>
+      <GridItem id={nodeId} key={nodeId}>
+        {props.children}
+      </GridItem>
     </Container>
   );
 }
--- src/components/organisms/EventCard/index.tsx
+++ src/components/organisms/EventCard/index.tsx
@@ -1,14 +1,25 @@
 import * as React from "react";
+import { v4 as uniqueId } from "uuid";
 
 import Card, { ICard } from "../../molecules/Card";
+
+import useDraggable from "../../../hooks/useDraggable";
 
 export interface IEventCard extends ICard {
   day?: number;
 }
 
 export default function EventCard(props: IEventCard) {
+  const { dragStart, dragOver, dragEnd } = useDraggable();
+
   return (
-    <div>
+    <div
+      id={uniqueId()}
+      draggable
+      onDragStart={dragStart}
+      onDragOver={dragOver}
+      onDragEnd={dragEnd}
+    >
       <Card {...props} />
     </div>
   );
