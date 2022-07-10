46c735a79fa7362beb058db4329870e83f4bf82c
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-10 15:13:50 -0300

    feat - new input validator hook

---
+++ src/hooks/useInputValidators.tsx
@@ -0,0 +1,46 @@
+import { Dispatch, SetStateAction } from "react";
+
+export default function useInputValidators() {
+  function validateLength(
+    value: string,
+    setErr: Dispatch<SetStateAction<boolean>>,
+    minLength: number
+  ) {
+    setErr(value.length <= minLength);
+  }
+
+  const validateEmail = (
+    value: string,
+    setErr: Dispatch<SetStateAction<boolean>>
+  ) => {
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    setErr(!value.match(regexEmail));
+  };
+
+  const validadeCompare = (
+    value: string,
+    compareToValue: string,
+    setErr: Dispatch<SetStateAction<boolean>>
+  ) => {
+    setErr(value !== compareToValue);
+  };
+
+  const validadePassword = (
+    value: string,
+    compareToValue: string,
+    setErr: Dispatch<SetStateAction<boolean>>,
+    minLength: number
+  ) => {
+    const compare = value === compareToValue;
+    const bigEnough = value.length >= minLength;
+    setErr(!(compare && bigEnough));
+  };
+
+  return {
+    validateLength,
+    validateEmail,
+    validadeCompare,
+    validadePassword,
+  };
+}
