c56ec60419c0691eacc88ebb15243d4251603f17
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-09 21:23:03 -0300

    feat - new calendar header component

---
+++ src/components/atoms/CalendarHeader/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import { Container } from "./styles";
+
+export default function CalendarHeader() {
+  return (
+    <Container>
+      <p>Seg</p>
+      <p>Ter</p>
+      <p>Qua</p>
+      <p>Qui</p>
+      <p>Sex</p>
+      <p>Sab</p>
+      <p>Dom</p>
+    </Container>
+  );
+}
