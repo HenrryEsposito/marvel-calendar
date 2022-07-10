2d990e24325dc2f79b156c005619ecc4d39a1ec6
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-09 21:25:37 -0300

    chore - implement useDate hook to test calendar

--- src/components/organisms/Calendar/index.tsx
+++ src/components/organisms/Calendar/index.tsx
@@ -1,77 +1,33 @@
-import * as React from "react";
+import React, { useMemo } from "react";
 import { v4 as uniqueId } from "uuid";
 
-import InfiniteScroll, { IIfinteItem } from "../InfiniteScroll";
+import CalendarHeader from "../../atoms/CalendarHeader";
+import InfiniteScroll from "../InfiniteScroll";
 import DaySlot from "../DaySlot";
-import Card from "../../molecules/Card";
 
-const mockItens: IIfinteItem[] = [
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={1} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={2} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={3} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={4} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={5} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={6} />,
-  },
-  {
-    key: uniqueId(),
-    children: (
-      <DaySlot
-        key={uniqueId()}
-        dayNumber={7}
-        children={
-          <Card
-            title="Titulo"
-            description="Lorem ipsum dolor sit amet, sed do eiusmod na aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          />
-        }
-      />
-    ),
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={8} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={9} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={10} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={11} />,
-  },
-  {
-    key: uniqueId(),
-    children: <DaySlot key={uniqueId()} dayNumber={12} />,
-  },
-];
+import useDateUtils from "../../../hooks/useDateUtils";
 
 export default function Calendar() {
+  const { getNearNextDaysByWeeks } = useDateUtils();
+
+  const mockedDays = useMemo(() => {
+    return getNearNextDaysByWeeks(4);
+  }, []);
+
+  const newMockedItemns = useMemo(() => {
+    return mockedDays.map((mockedDay) => {
+      return {
+        key: uniqueId(),
+        children: <DaySlot key={uniqueId()} dayNumber={mockedDay.getDate()} />,
+      };
+    });
+  }, [mockedDays]);
+
   return (
     <div className="calendar-base">
+      <CalendarHeader />
       <InfiniteScroll
-        items={mockItens}
+        items={newMockedItemns}
         onReachBottom={() => console.log("bottom")}
         onReachTop={() => console.log("top")}
       ></InfiniteScroll>
