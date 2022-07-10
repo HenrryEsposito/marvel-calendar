dcdecf359bf831a1d4e883fb342146ad07c64473
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-10 11:42:40 -0300

    feat - new login modal component

---
+++ src/components/organisms/LoginModal/index.tsx
@@ -0,0 +1,74 @@
+import React, { useRef } from "react";
+import {
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  OutlinedInput,
+  InputLabel,
+  FormControl,
+  Button,
+} from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
+
+import Modal, { IModalHandles } from "../Modal";
+import PasswordInput from "../../molecules/PasswordInput";
+
+import { Container } from "./styles";
+
+export interface IModal {}
+
+const LoginModal: React.ForwardRefRenderFunction<IModalHandles, IModal> = (
+  _,
+  forwardedRef
+) => {
+  const loginInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Modal ref={forwardedRef}>
+      <Container>
+        <DialogTitle id="alert-dialog-title">
+          Entre com a sua conta.
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <Grid container spacing={2} p={1}>
+              <Grid item xs={6}>
+                <FormControl>
+                  <InputLabel htmlFor="password-input">E-mail</InputLabel>
+                  <OutlinedInput
+                    inputRef={loginInputRef}
+                    label="E-mail"
+                    placeholder="tonystark@starkco.com"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <PasswordInput ref={passwordInputRef} />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="outlined"
+                  size="large"
+                  endIcon={<LoginIcon />}
+                  onClick={() => {
+                    console.log(
+                      "modal login",
+                      loginInputRef.current?.value,
+                      passwordInputRef.current?.value
+                    );
+                  }}
+                >
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </DialogContentText>
+        </DialogContent>
+      </Container>
+    </Modal>
+  );
+};
+
+export default React.forwardRef(LoginModal);
