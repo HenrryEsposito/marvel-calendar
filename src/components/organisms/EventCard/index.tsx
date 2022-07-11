3f37fa842aae8e7590b992215c36a8120eec342c
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-11 19:28:58 -0300

    feat - show event char list on card

--- src/components/organisms/EventCard/index.tsx
+++ src/components/organisms/EventCard/index.tsx
@@ -2,25 +2,35 @@ import * as React from "react";
 import { v4 as uniqueId } from "uuid";
 
 import Card, { ICard } from "../../molecules/Card";
+import List from "../../molecules/List";
 
 import useDraggable from "../../../hooks/useDraggable";
 
+import { Container } from "./styles";
+
+import { ICharacterInfo } from "../../../store/event/types";
 export interface IEventCard extends ICard {
   day?: number;
+  eventId: string;
+  chars: ICharacterInfo[];
 }
 
 export default function EventCard(props: IEventCard) {
   const { dragStart, dragOver, dragEnd } = useDraggable();
 
   return (
-    <div
+    <Container
       id={uniqueId()}
       draggable
-      onDragStart={dragStart}
+      onDragStart={(e) => {
+        dragStart(e, props.eventId);
+      }}
       onDragOver={dragOver}
       onDragEnd={dragEnd}
     >
-      <Card {...props} />
-    </div>
+      <Card {...props}>
+        <List items={props.chars} />
+      </Card>
+    </Container>
   );
 }
---
+++ src/components/organisms/EventCard/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: white;
+  border: 2px solid #bfbfbf;
+  border-radius: 20px;
+`;
