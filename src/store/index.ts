6a84e036ca86cbf6543743fb404fd9a75d0b4249
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-11 00:30:35 -0300

    chore - include event reducer in root store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,9 +3,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import { userReducer } from "./user/reducers";
 import { appReducer } from "./app/reducers";
+import { eventReducer } from "./event/reducers";
 
 import { IUserStore } from "./user/types";
 import { IAppStore } from "./app/types";
+import { IEventStore } from "./event/types";
 
 export interface IAction {
   type: any;
@@ -15,10 +17,15 @@ export interface IAction {
 export interface rootStore {
   userStore: IUserStore;
   appStore: IAppStore;
+  eventStore: IEventStore;
 }
 
 const store = createStore(
-  combineReducers({ userStore: userReducer, appStore: appReducer })
+  combineReducers({
+    userStore: userReducer,
+    appStore: appReducer,
+    eventStore: eventReducer,
+  })
 );
 
 export default store;
