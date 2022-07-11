af92cb80756684dc4d986f644c1a6c5d06b8ea03
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-11 09:36:19 -0300

    fix - event interface definitions

--- src/store/event/types.ts
+++ src/store/event/types.ts
@@ -2,16 +2,17 @@ export interface IEventStore {
   events: IEventInfo[];
 }
 
+export interface ICharacterInfo {
+  id: string;
+  name: string;
+  thumb: string;
+  year: number;
+}
+
 export interface IEventInfo {
   id: string;
   title: string;
+  description: string;
   date: string;
-  characters: [
-    {
-      id: string;
-      name: string;
-      thumb: string;
-      year: number;
-    }
-  ];
+  characters: ICharacterInfo[];
 }
