6ec2252bd0f3bcfd15820366092afb30ec2e300c
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-10 23:08:57 -0300

    feat - implement new app step on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Calendar from "./components/organisms/Calendar";
 import Splash from "./components/organisms/Splash";
 
-import { IAppStore } from "./store/app/types";
+import { rootStore } from "./store";
 import { appStep } from "./store/app/types";
 
+import useLocalStorage from "./hooks/useLocalStorage";
+import useAuth from "./hooks/useAuth";
+
 function App() {
-  const currentAppStep = useSelector((state: IAppStore) => state.appStep);
+  const { setItem, getItem } = useLocalStorage();
+  const { refreshLogin } = useAuth();
+
+  useEffect(() => {
+    refreshLogin();
+  }, []);
+
+  const currentAppStep = useSelector(
+    (state: rootStore) => state.appStore.appStep
+  );
 
   return (
     <div className="App">
