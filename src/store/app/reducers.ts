9ff03884f962bd1f280fb925ab078cab4bd6ad63
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-10 23:05:19 -0300

    refactor - implement combine reducers

--- src/store/app/reducers.ts
+++ src/store/app/reducers.ts
@@ -1,11 +1,10 @@
-import { createStore } from "@reduxjs/toolkit";
 import { IAction } from "../";
 import { IAppStore, appStep } from "./types";
 import { actionType } from "./actions";
 
 const INITIAL_STATE: IAppStore = { appStep: appStep.SPLASH };
 
-function appReducer(state = INITIAL_STATE, action: IAction) {
+export function appReducer(state = INITIAL_STATE, action: IAction) {
   switch (action.type) {
     case actionType.SET_CURRENT_STEP:
       return { ...state, appStep: action.payload };
@@ -14,7 +13,3 @@ function appReducer(state = INITIAL_STATE, action: IAction) {
       return state;
   }
 }
-
-const store = createStore(appReducer);
-
-export default store;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,24 @@
+import { createStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+
+import { userReducer } from "./user/reducers";
+import { appReducer } from "./app/reducers";
+
+import { IUserStore } from "./user/types";
+import { IAppStore } from "./app/types";
+
 export interface IAction {
   type: any;
   payload: any;
 }
+
+export interface rootStore {
+  userStore: IUserStore;
+  appStore: IAppStore;
+}
+
+const store = createStore(
+  combineReducers({ userStore: userReducer, appStore: appReducer })
+);
+
+export default store;
