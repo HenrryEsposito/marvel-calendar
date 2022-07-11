f533ef874739eb7e68b0acb1daa476ce600385b8
Henrry Esposito <henrrygesposito@gmail.com>
2022-07-11 11:43:01 -0300

    feat - create event modal marvel api integration

--- src/components/organisms/CreateEventModal/index.tsx
+++ src/components/organisms/CreateEventModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   DialogContent,
   DialogContentText,
@@ -9,14 +9,15 @@ import {
   FormControl,
   Button,
 } from "@mui/material";
-import LoginIcon from "@mui/icons-material/Login";
 import { v4 as uniqueId } from "uuid";
 
+import useMarvelAPI from "../../../hooks/useMarvelAPI";
 import useEvents from "../../../hooks/useEvents";
 
 import Modal, { IModalHandles } from "../Modal";
+import List from "../../molecules/List";
 
-import { Container } from "./styles";
+import { ICharacterInfo } from "../../../store/event/types";
 
 export interface ICreateEventModal {}
 
@@ -27,73 +28,107 @@ const CreateEventModal: React.ForwardRefRenderFunction<
   const titleInputRef = useRef<HTMLInputElement>(null);
   const descriptionInputRef = useRef<HTMLInputElement>(null);
   const dateInputRef = useRef<HTMLInputElement>(null);
+  const charInputRef = useRef<HTMLInputElement>(null);
 
+  const { getCharacter } = useMarvelAPI();
   const { createNewEvent } = useEvents();
+
+  const [charList, setCharList] = useState<ICharacterInfo[]>([]);
+
+  async function addCharacter() {
+    const charToSearch = charInputRef.current?.value;
+
+    let char: ICharacterInfo | null = null;
+    if (!!charToSearch)
+      char = (await getCharacter(charToSearch)) as ICharacterInfo;
+
+    if (!char) return alert("Nenhum personagem encontrado com esse nome!");
+
+    setCharList((prev) => [...prev, char as ICharacterInfo]);
+  }
 
   return (
     <Modal ref={forwardedRef}>
-      <Container>
-        <DialogTitle id="alert-dialog-title">Criar um evento.</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <Grid container spacing={2} p={1}>
-              <Grid item xs={6}>
-                <FormControl>
-                  <InputLabel htmlFor="title-input">Título</InputLabel>
-                  <OutlinedInput
-                    id="title-input"
-                    inputRef={titleInputRef}
-                    label="Título"
-                  />
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={6}>
-                <FormControl>
-                  <InputLabel htmlFor="description-input">Descrição</InputLabel>
-                  <OutlinedInput
-                    id="description-input"
-                    inputRef={descriptionInputRef}
-                    label="Descrição"
-                  />
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={6}>
-                <FormControl>
-                  <InputLabel htmlFor="date-input">
-                    Data de Lançamento
-                  </InputLabel>
-                  <OutlinedInput
-                    id="date-input"
-                    inputRef={dateInputRef}
-                    label="Data de Lançamento"
-                  />
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <Button
-                  variant="outlined"
-                  size="large"
-                  endIcon={<LoginIcon />}
-                  onClick={() => {
-                    createNewEvent({
-                      id: uniqueId(),
-                      title: titleInputRef.current?.value || "",
-                      description: descriptionInputRef.current?.value || "",
-                      date: dateInputRef.current?.value || "",
-                      characters: [],
-                    });
-                  }}
-                >
-                  Criar
-                </Button>
-              </Grid>
+      <DialogTitle id="alert-dialog-title">Criar um evento.</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <Grid container spacing={2} p={1}>
+            <Grid item xs={6}>
+              <FormControl>
+                <InputLabel htmlFor="title-input">Título</InputLabel>
+                <OutlinedInput
+                  id="title-input"
+                  inputRef={titleInputRef}
+                  label="Título"
+                />
+              </FormControl>
             </Grid>
-          </DialogContentText>
-        </DialogContent>
-      </Container>
+
+            <Grid item xs={6}>
+              <FormControl>
+                <InputLabel htmlFor="date-input">Data de Lançamento</InputLabel>
+                <OutlinedInput
+                  id="date-input"
+                  inputRef={dateInputRef}
+                  label="Data de Lançamento"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="description-input">Descrição</InputLabel>
+                <OutlinedInput
+                  id="description-input"
+                  inputRef={descriptionInputRef}
+                  label="Descrição"
+                  rows={5}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl>
+                <InputLabel htmlFor="date-input">Personagem</InputLabel>
+                <OutlinedInput
+                  id="search-character-input"
+                  inputRef={charInputRef}
+                  label="Personagem"
+                  onChange={(e) => console.log(e.target.value)}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Button variant="outlined" size="large" onClick={addCharacter}>
+                Adicionar Personagem
+              </Button>
+            </Grid>
+
+            <Grid item xs={12}>
+              <List items={charList}></List>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button
+                variant="outlined"
+                size="large"
+                onClick={() => {
+                  createNewEvent({
+                    id: uniqueId(),
+                    title: titleInputRef.current?.value || "",
+                    description: descriptionInputRef.current?.value || "",
+                    date: dateInputRef.current?.value || "",
+                    characters: charList,
+                  });
+                }}
+              >
+                Criar Evento
+              </Button>
+            </Grid>
+          </Grid>
+        </DialogContentText>
+      </DialogContent>
     </Modal>
   );
 };
